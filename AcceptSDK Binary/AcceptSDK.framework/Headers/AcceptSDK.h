9f9b25ebdf4422f6f12241981f338a117a4912ed
Rakesh <rramamur@visa.com>
2016-07-25 12:26:05 +0530

    [accept-sdk-ios]: Adding AcceptSDK Universal  Binary for Binary only pod support.

---
+++ AcceptSDK Binary/AcceptSDK.framework/Headers/AcceptSDK.h
@@ -0,0 +1,19 @@
+//
+//  AcceptSDK.h
+//  AcceptSDK
+//
+//  Created by Ramamurthy, Rakesh Ramamurthy on 7/15/16.
+//  Copyright © 2016 Ramamurthy, Rakesh Ramamurthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AcceptSDK.
+FOUNDATION_EXPORT double AcceptSDKVersionNumber;
+
+//! Project version string for AcceptSDK.
+FOUNDATION_EXPORT const unsigned char AcceptSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcceptSDK/PublicHeader.h>
+
+
